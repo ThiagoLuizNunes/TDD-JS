e71283a95cd94ded85e6c0afcb0402d724f5b47d
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-12-20 20:51:16 -0300

    add practice03

---
+++ src/practice03.js
@@ -0,0 +1,17 @@
+const city = 'Jampa';
+const someThing = 'beach';
+const anotherThing = 'surf';
+
+function green(template, ...values) {
+  return template.reduce((accumulater, currentValue, i) => {
+    return `
+      ${accumulater} 
+      <span class="green">${values[i - 1].toUpperCase()}</span>
+      ${currentValue}
+    `
+  });
+}
+
+const sentence = green`I love ${city} because there is ${someThing} and ${anotherThing}`;
+
+document.body.innerHTML = sentence;
