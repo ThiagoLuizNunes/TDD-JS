7520696a31ec4795e12b865aaa77ec7460d9e8a9
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-12-20 20:58:27 -0300

    fix style guide

--- src/practice03.js
+++ src/practice03.js
@@ -3,13 +3,11 @@ const someThing = 'beach';
 const anotherThing = 'surf';
 
 function green(template, ...values) {
-  return template.reduce((accumulater, currentValue, i) => {
-    return `
-      ${accumulater} 
-      <span class="green">${values[i - 1].toUpperCase()}</span>
-      ${currentValue}
-    `
-  });
+  return template.reduce((accumulater, currentValue, i) => (
+    `${accumulater}
+     <span class="green">${values[i - 1].toUpperCase()}</span>
+     ${currentValue}`
+  ));
 }
 
 const sentence = green`I love ${city} because there is ${someThing} and ${anotherThing}`;
