f6ed9ff2816c02ea9ebb694b009c70aa322414f8
thiagoluiznunes <thiago@assistatecnologia.com.br>
2018-09-14 20:04:32 +0000

    fix fizzbuzz challenge

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,19 +1,13 @@
-const fizz = (num) => {
+const fizzBuzz = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  }
   if (num % 3 === 0) {
     return 'Fizz';
   }
-};
-
-const buzz = (num) => {
   if (num % 5 === 0) {
     return 'Buzz';
   }
 };
 
-const fizzBuzz = (num) => {
-  if (num % 3 === 0 && num % 5 === 0) {
-    return 'Fizz Buzz';
-  }
-};
-
-export { fizz, buzz, fizzBuzz };
+export default fizzBuzz;
--- tests/fizzbuzz.spec.js
+++ tests/fizzbuzz.spec.js
@@ -1,33 +1,30 @@
 import { expect } from 'chai';
-import { fizz, buzz, fizzBuzz } from '../src/fizzbuzz';
-
+import fizzBuzz from '../src/fizzbuzz';
 
 describe('FizzBuzz', () => {
-  
-  describe('Smoke tests', () => {
 
-    it('should exist the lib fizzbuzz', () => {
-      expect(fizz).to.exist;
-    });
-
-    it('should exist the lib fizzbuzz', () => {
-      expect(buzz).to.exist;
-    });
+  describe('Smoke test', () => {
 
     it('should exist the lib fizzbuzz', () => {
       expect(fizzBuzz).to.exist;
     });
   });
 
-  describe('Number dived by 3', () => {
-    expect(fizz(3)).to.equal('Fizz');
-  });
-  
-  describe('Number dived by 5', () => {
-    expect(buzz(5)).to.equal('Buzz');
-  });
+  describe('FizzBuzz Cases', () => {
 
-  describe('Number dived by 3 and 5', () => {
-    expect(fizzBuzz(15)).to.equal('Fizz Buzz');
+    it('should return `Fizz` when multiple of 3', () => {
+      expect(fizzBuzz(3)).to.equal('Fizz');
+      expect(fizzBuzz(6)).to.equal('Fizz');
+    });
+
+    it('should return `Buzz` when multiple of 5', () => {
+      expect(fizzBuzz(5)).to.equal('Buzz');
+      expect(fizzBuzz(10)).to.equal('Buzz');
+    });
+
+    it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+      expect(fizzBuzz(15)).to.equal('FizzBuzz');
+      expect(fizzBuzz(30)).to.equal('FizzBuzz');
+    });
   });
 });
