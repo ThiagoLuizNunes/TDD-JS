5aede95ed142778a125814dc44f9e956e709d76d
thiagoluiznunes <thiago@assistatecnologia.com.br>
2018-09-14 12:06:46 +0000

    create first test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,3 +1,18 @@
-describre('Main', () => {
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should to say hello dude', () => {
 
+      });
+    });
+    context('Case 2', () => {
+      it('should to say hi dhana', () => {
+
+      })
+    });
+  });
+
+  describe('Method A', () => {
+
+  });
 });
