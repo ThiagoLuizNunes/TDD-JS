8492aeb0223ae5fb83f4bda3846b4a221f6810b9
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2019-01-03 19:27:17 +0000

    create prototype inheritance example

---
+++ es6/prototype-inheritance.js
@@ -0,0 +1,17 @@
+function Animal(kind, sound) {
+  this.kind = kind;
+  this.sound = sound;
+}
+
+Animal.prototype.hello = function() {
+  console.log(`${this.sound} I'm a ${this.kind}`);
+};
+
+const dog = new Animal('dog', 'auau');
+const cat = new Animal('cat', 'miau');
+
+console.log(dog);
+console.log(cat);
+dog.hello();
+cat.hello();
+
