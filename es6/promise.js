4222f6cf99fec1a6624fdf4433fe2ee04217e5e4
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2019-01-03 18:51:02 +0000

    create example promise all

---
+++ es6/promise.js
@@ -0,0 +1,17 @@
+const currency = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ currency: 'euro', value: 3.50 });
+  }, 2000);
+});
+
+const countries = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(['Ireland', 'Englad', 'Scotland']);
+  }, 600);
+});
+
+Promise
+  .all([currency, countries])
+  .then((responses) => {
+    console.log(responses);
+  });
