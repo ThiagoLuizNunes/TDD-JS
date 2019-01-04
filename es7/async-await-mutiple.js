8ade347ff70606e84ec26b8a3d5c0673630307bc
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2019-01-04 17:51:02 +0000

    create async await examples

---
+++ es7/async-await-mutiple.js
@@ -0,0 +1,8 @@
+async function getUsers(users) {
+  const promises = users.map(user => fetch(`https://api.github.com/users/${user}`).then(r => r.json()));
+
+  const people = await Promise.all(promises);
+  console.log(people.map(p => p.company));
+}
+
+getUsers(['willianjusten', 'thiagoluiznunes', 'caiomcg', '4ndr6']);
--- es7/async-await.js
+++ es7/async-await.js
@@ -1,12 +1,14 @@
 async function getPosts() {
-  const posts = await fetch('https://willianjusten.com.br/search.json');
-  const data = await posts.json();
+  try {
+    const posts = await fetch('https://willianjusten.com.br/search.json');
+    const data = await posts.json();
 
-  return data.map((post) => {
-    console.log(post.title);
-  });
-
-  console.log(data);
+    return data.map((post) => {
+      console.log(post.title);
+    });
+  } catch (error) {
+    console.error('We got a error:', error);  
+  }
 }
 
 getPosts();
